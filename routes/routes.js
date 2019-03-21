a86a12bd498646e2de99100958bfc83ab548dfd3
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2019-03-21 16:30:41 +0000

    initial project commit

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+// import other routes
+const userRoutes = require('./users');
+
+const appRouter = (app, fs) => {
+
+    // default route
+    app.get('/', (req, res) => {
+        res.send('welcome to the development api-server');
+    });
+
+    // // other routes
+    userRoutes(app, fs);
+
+};
+
+module.exports = appRouter;
---
+++ routes/users.js
@@ -0,0 +1,91 @@
+
+const userRoutes = (app, fs) => {
+
+    // variables
+    const dataPath = './data/users.json';
+
+    // helper methods
+    const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+        fs.writeFile(filePath, fileData, encoding, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            callback();
+        });
+    };
+
+    // READ
+    app.get('/users', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        });
+    });
+
+    // CREATE
+    app.post('/users', (req, res) => {
+
+        readFile(data => {
+            const newUserId = Object.keys(data).length + 1;
+
+            // add the new user
+            data[newUserId] = JSON.parse(req.body.data);
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send('new user added');
+            });
+        },
+            true);
+    });
+
+
+    // UPDATE
+    app.put('/users/:id', (req, res) => {
+
+        readFile(data => {
+
+            // add the new user
+            const userId = req.params["id"];
+            data[userId] = JSON.parse(req.body.data);
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send(`users id:${userId} updated`);
+            });
+        },
+            true);
+    });
+
+
+    // DELETE
+    app.delete('/users/:id', (req, res) => {
+
+        readFile(data => {
+
+            // add the new user
+            const userId = req.params["id"];
+            delete data[userId];
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send(`users id:${userId} removed`);
+            });
+        },
+            true);
+    });
+};
+
+module.exports = userRoutes;
